77486e6df229c69981c1a76ea518d2c6097c88cd
NG-Joseph <jcool4liv@gmail.com>
2020-05-13 12:21:22 +0100

    progress update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,64 @@
-//import logger, {appName, dummyFunction} from './tools' 
-//logger(`Welcome! The application name is "${appName}". There is a function that returns "${dummyFunction()}".` ); 
+//Using var and function
 
-import logger, { multiplier1 } from "./tools";
+/* 
+import logger from './tools'  
+logger("Welcome! We are now having fun with modularity"); 
+*/
+// using Back -ticks
+import logger, 
+{appName, dummyFunction, genericFunction,multiplier, multiplier2, timeout, timeoutwithpromise,coordinateGenerator} from './tools'; 
+logger(`Welcome! The application name is "${appName}". There is a function that returns "${dummyFunction()}".`); 
+logger  (genericFunction());
 
-//import logger, {genericFunction} from './tools' 
-//logger(`This is a function that returns "${genericFunction()}".` ); 
+logger  (multiplier(44,23));
+
+logger  (multiplier2(1,2,3,4,5,6,7));
 try{
-
-
-logger(multiplier1(1,9,3,53));
-}catch(error){
-    logger(error.message);
+  let result = multiplier2(3,5);
+  logger(result);
 }
+catch(Error)
+{
+    logger(Error.message);
+    
+}
+
+//Typescript
+
+import log, {Person} from './tools'; 
+//Accessing object properties
+ let ﬁrstName = "Pius";
+  let lastName = "Onobhayedo";
+   let gender = "male";
+    let height = 1.7;
+     let hobbies = ['Tennis','Programming'];
+      let birthday = new Date(1900,4,5);
+       let isAlive = true; 
+let person1 = new Person(ﬁrstName, lastName, gender, height,      hobbies, birthday, isAlive) 
+log(`Person 1 is ${person1.ﬁrstName} whose height is ${person1.height} and birthday is ${person1.birthday.toLocaleDateString()}`); 
+
+//Define the function that will return new Promise that wraps the asynchronous call to setTimeout()
+const promiseAwareTimeout = (milliseconds=1000) => { //The function expects milliseconds to be passed. If not passed, milliseconds parameter defaults to 1000
+    return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+    resolve(`Timeout of ${milliseconds} milliseconds is over`); //send out a success feedback with data using resolve
+    }, milliseconds) //set timeout for passed milliseconds. Defaults to 1000
+    });
+   }
+   //define a function that uses our Promise executor
+   const usePromiseAwareTimeout = async (milliseconds) => {
+    log('About to call timeout')
+    try{
+    log(await promiseAwareTimeout(milliseconds));
+    }catch(error){
+    log(error);
+    }
+   };
+   //call the async function.
+   usePromiseAwareTimeout(3000); 
+
+
+timeout(7000);
+timeoutwithpromise(7000);
+//logger(coordinateGenerator(2,3,4,5,6));
+
--- src/tools.js
+++ src/tools.js
@@ -8,7 +8,7 @@
         export default logger; */
 
         //Lexical block scoping
-        const logger = output => {
+    /*    const logger = output => {
             console.log(output);
         }
 
@@ -43,4 +43,161 @@
                          return product;
         }
              
-        export default logger;
+        export default logger;*/
+        
+//Using Var and Fucntion
+
+/*var logger = function(output)
+{   
+    console.log(output) 
+}  
+export default logger;
+*/
+
+/*
+var logger = function(output)
+{ 
+    console.log(output);
+} 
+export var appName = "ES6 Review"; 
+export var dummyFunction = function()
+{  
+    return "I am a dummy function";  
+}
+export default logger;
+*/
+
+//Parameter scenarios- new styles
+/*
+var logger = output => 
+{
+    console.log(output);
+}
+export var appName = "ES6 Review"; 
+
+export var dummyFunction = ()=>
+{
+    return"I am a dummy function";
+}
+*/
+
+//Lexical Blcok scoping
+// Using Const or Let
+
+const logger = output => {
+
+    console.log(output);
+}
+export let appName = "ES6 Review"; 
+
+export const dummyFunction = ()=>{
+
+    return"I am a dummy function";
+}
+
+/* Spread Operators*/
+//Destructuring Assignment
+export let genericFunction = () => {
+    const languages = ['python','javascript','c#', 'c++']
+    const [firstLang,secondLang,...Otherlanguages] = languages;
+    return `first language is ${firstLang} and the second Language is ${secondLang} and the rest are ${Otherlanguages}`;
+}
+
+export let multiplier = (a,b=1) => {
+    return(a*b);
+}
+
+export const multiplier2 = (...numberstomultiply)=>
+{
+    if(numberstomultiply.length < 2){
+        throw new Error("Illegal Argument Count,the number of  Arguments passed must be greater than one");
+    }
+   let product = 1;
+   for(let number of numberstomultiply){
+       product = product * number;
+   }
+   return product;
+}
+ export default logger;
+//TypeScript
+ export class Person
+ { 
+    constructor(ﬁrstName, lastName, gender, height, hobbies, birthday, isAlive)
+    {  this.ﬁrstName = ﬁrstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.height = height;
+        this.hobbies = hobbies;         
+        this.birthday = birthday;         
+        this.isAlive = isAlive;
+    } 
+};
+
+export const timeout = millisecond =>{
+    setTimeout(()=>
+{        
+   return logger("Timeout is over by "+ millisecond/1000+" seconds"); 
+   //send out a success feedback with data using resolve 
+}, 
+    millisecond) 
+
+}
+
+/*export const timeoutwithpromise = (millisecond) =>{
+        new Promise ((resolve, reject) =>{
+        setTimeout(()=>{  
+            resolve("Timeout is over by "+ millisecond/1000+" seconds");
+            reject("Timeout is over but I am upset. You should not have invoked a timeout in the ﬁrst place. Hence I am sending a <em> reject </em> instead of a <em>resolve</em>!"); 
+            //send out a reject feedback     
+               }, 1000)    
+              }).then((data) => { 
+                        logger(`${data}`);
+                        //This should output "Timeout is over"    
+                        }).catch((error) => {
+                          //This will only be reached it the asynchronous function returned a reject statement.        
+                           return logger(`Error message received: ${error}`);   
+                           }).then(() =>{       
+                               return logger(`This function is executed after success or failure is returned`) 
+                                 }, millisecond)
+}*/
+
+
+    export const genericFunction1 = (m, c, ...x) => {    
+        //This function returns an array of {x,y} objects for all the x arguments passed.
+
+        //Map the array of x into an array of {x,y} object, with the y value calculated each time.    
+         let coordinates = x.map((x) => {        
+              return {'x': x, 'y' : (m*x) + c};    
+            })   
+              //loop through our array of {x,y} and prepare the output string to be returned. We can do this with the forEach() method of array object or use the for…of loop introduced in ES6 as shown below     
+              let output = 'The (x,y) values are as follows: '     
+              for (let coordinate of coordinates){         
+                   let xy = `(${coordinate.x},${coordinate.y})`         
+                   output+=xy;     
+               }   
+                //Previous style…(Going forward, we shall only be using the new for…of loop in such scenario)
+               //coordinates.forEach((coordinate)=>{ 
+               //     let xy = `(${coordinate.x},${coordinate.y})`;    
+                //    output+=xy;     //})     
+                          return output; 
+                       } 
+   
+         
+   export const coordinateGenerator = (m, c, ...xValues) => {
+       //This function returns an array of {x,y} objects for all the x arguments passed to it.
+       
+       let coordinates = xValues.map(
+           (x) =>{
+               return {'x':x, 'y':(m*x)+ c};
+           }
+       );
+       let output = 'The (x,y) values are as follows '
+       for (let coordinate of coordinates){
+           let xy = `(${coordinate.x}, ${coordinate.y})`;
+           //output = output + xy  //This is the same as below.
+           output +=xy;
+       }
+       return output;
+   
+   }
+   
